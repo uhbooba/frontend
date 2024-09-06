1e9ecfe2e80ec92cc4b2636048520e8ab82cff43
김인엽 <inyub98@gmail.com>
2024-09-06 18:02:47 +0900

    :sparkles: feat : init backend project - 아직 설정파일 필요

---
+++ backend/external-service/src/test/java/com/ssafy/uhbooba/externalservice/ExternalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.uhbooba.externalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
