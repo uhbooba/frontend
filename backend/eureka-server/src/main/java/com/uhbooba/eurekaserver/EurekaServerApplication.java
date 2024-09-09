2d36c488bcb638585f411805413725d583092480
김인엽 <inyub98@gmail.com>
2024-09-09 17:38:31 +0900

    :hammer: Chore : 프로젝트 구조 변경(groupId change), 도커 설정 추가

--- backend/eureka-server/src/main/java/com/uhbooba/eurekaserver/EurekaServerApplication.java
+++ backend/eureka-server/src/main/java/com/uhbooba/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.ssafy.uhbooba.eurekaserver;
+package com.uhbooba.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/external-service/src/main/java/com/uhbooba/externalservice/ExternalServiceApplication.java
+++ backend/external-service/src/main/java/com/uhbooba/externalservice/ExternalServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ssafy.uhbooba.externalservice;
+package com.uhbooba.externalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
