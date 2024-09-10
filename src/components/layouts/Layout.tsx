57f05af96c5a39b49f634d48c8bc6d085eb5c74f
Thoughtful-Lover <isso8336@gmail.com>
2024-09-10 19:06:53 +0900

    Feat: Layout first draft

---
+++ src/components/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className='min-h-screen bg-gray-100 flex justify-center'>
+            <div className='w-full max-w-screen-lg px-4'>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+
+export default Layout;
