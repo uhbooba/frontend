e57532d84d383cff6cb7f263cbcd7205843cb941
Thoughtful-Lover <isso8336@gmail.com>
2024-09-11 17:15:13 +0900

    :wrench: Chore: some setting changed
    <body>
    - <Scope>
    	- discription

--- frontend/src/components/layouts/Layout.tsx
+++ frontend/src/components/layouts/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className='min-h-screen bg-gray-100 flex justify-center'>
-            <div className='w-full max-w-screen-lg px-4'>
+            <div className='w-full max-w-screen-md px-4'>
                 {children}
             </div>
         </div>
