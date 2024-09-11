b4e81126863c532a014e652bc8fc332c86461cc8
swybean <swybean@gmail.com>
2024-09-11 16:04:04 +0900

    :sparkles: Feat: add Savings
    <body>
    - Savings.tsx
    	- 적금 입장 화면 구성

--- src/pages/Savings.tsx
+++ src/pages/Savings.tsx
@@ -1,6 +1,6 @@
 // import Button from '../components/common/Button';
 // import XButton from '@/components/common/XButton';
-// import BackButton from '@/components/common/BackButton';
+import BackButton from '@/components/common/BackButton';
 // import CheckButton from '@/components/common/CheckButton';
 // import Layout from '@/components/common/layouts/Layout';
 
@@ -8,7 +8,17 @@ const Savings = () => {
   console.log('메인 컴포넌트 렌더링 확인영');
   return (
     <div>
-      <span> 여기는 적금 가입 페이지 입니다.</span>
+      <div className='bg-gray-400 text-center'>
+        <span>여기는 휴대폰 상단 상태바</span>
+      </div>
+
+      {/* 상단바 */}
+      <div className='flex items-center justify-between border-b-4 bg-white py-4'>
+        <BackButton className='ml-4' />
+        <span className='absolute left-1/2 -translate-x-1/2 transform text-xl font-bold'>
+          적금 가입
+        </span>
+      </div>
     </div>
   );
 };
