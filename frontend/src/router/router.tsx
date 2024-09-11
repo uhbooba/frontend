7a161b8bb6c84fa136a5c874ee5c01f45f5ce36e
swybean <swybean@gmail.com>
2024-09-11 18:03:19 +0900

    :sparkles: Feat: Deposit-signup-page
    <body>
    - Deposit_Signup.tsx
    	- 상품 가입 개인정보 입력 페이지
    - Deposit_Signup.tsx
    	- 상품 가입 동의 페이지

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import Main from '@/pages/Main';
-import DepositMission from '@/pages/Deposit_Mission';
-import Savings from '@/pages/Savings';
-import DepositExplain from '@/pages/Deposit_explain';
+import Savings from '@/pages/savings/Savings';
+import DepositMission from '@/pages/deposit/Deposit_Mission';
+import DepositExplain from '@/pages/deposit/Deposit_Explain';
+import DepositSignup from '@/pages/deposit/Deposit_Signup';
+import DepositSignup2 from '@/pages/deposit/Deposit_Signup2';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +16,8 @@ const router = createBrowserRouter([
       { path: 'deposit', element: <DepositMission /> },
       { path: 'savings', element: <Savings /> },
       { path: 'explain', element: <DepositExplain /> },
+      { path: 'deposit/signup', element: <DepositSignup /> },
+      { path: 'deposit/signup2', element: <DepositSignup2 /> },
     ],
   },
 ]);
