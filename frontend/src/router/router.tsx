c638acda9c567c5cd925db6b910c4bf6210083ae
yunva17 <yunva17@naver.com>
2024-09-13 15:36:56 +0900

    ✨Feat: update router & errorText Component

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -4,6 +4,8 @@ import Main from '@/pages/Main';
 import DepositMission from '@/pages/Deposit_Mission';
 import Savings from '@/pages/Savings';
 import DepositExplain from '@/pages/Deposit_explain';
+import Login from '@/pages/auth/Login';
+import Signup from '@/pages/auth/Signup';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +16,8 @@ const router = createBrowserRouter([
       { path: 'deposit', element: <DepositMission /> },
       { path: 'savings', element: <Savings /> },
       { path: 'explain', element: <DepositExplain /> },
+      { path: 'signup', element: <Signup /> },
+      { path: 'login', element: <Login /> },
     ],
   },
 ]);
