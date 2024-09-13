859d57f867ba778672ca4d9b54560ee28b666b12
yunva17 <yunva17@naver.com>
2024-09-13 15:35:15 +0900

    ✨Feat : login page

---
+++ frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,26 @@
+import Button from '@/components/common/buttons/Button';
+import { Input } from '@/components/common/Input';
+import { useState } from 'react';
+
+const Login = () => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+      <Input
+        label='아이디'
+        value={userId}
+        onChange={(e) => setUserId(e.target.value)}
+        className='mb-5'
+      />
+      <Input label='비밀번호' value={password} type='password' />
+      <div className='mt-3 flex flex-col justify-center'>
+        <Button label='로그인' className='mb-3' />
+        <Button label='회원가입' />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
