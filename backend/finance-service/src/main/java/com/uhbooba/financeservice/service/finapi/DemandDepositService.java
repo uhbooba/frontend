81c0edcbc48df95951121d99a05ce924e3dc7453
김인엽 <inyub98@gmail.com>
2024-09-19 18:02:37 +0900

    :sparkles: Feat : Add Demand Deposit Service

---
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/service/finapi/DemandDepositService.java
@@ -0,0 +1,52 @@
+package com.uhbooba.financeservice.service.finapi;
+
+import com.uhbooba.financeservice.util.finapi.FinOpenApiHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DemandDepositService {
+
+    private final FinOpenApiHandler finOpenApiHandler;
+
+    public void getBankCodes() {
+
+    }
+
+    public void createDemandDepositAccount() {
+
+    }
+
+    public void getDemandDepositAccount() {
+
+    }
+
+    public void getDemandDepositAccounts() {
+
+    }
+
+    public void getDemandDepositAccountHolderName() {
+
+    }
+
+    public void getDemandDepositAccountBalance() {
+
+    }
+
+    public void depositDemandDepositAccount() {
+
+    }
+
+    public void transferDemandDepositAccount() {
+
+    }
+
+    public void getTransactionHistories() {
+
+    }
+
+    public void getTransactionHistory() {
+
+    }
+}
