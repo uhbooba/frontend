aa92c388c1478d33571ef19299f3af4a971fd2f1
yunva17 <yunva17@naver.com>
2024-09-19 09:35:38 +0900

    💄Style : 색상 컬러 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        primary: '#FFAF2A',
+        'red-main': '#FF3232',
+        'green-main': '#3AC73B',
+      },
       padding: {
         38: '9.5rem',
         15: '3.5rem',
