4fc662deb8685205cb16cc1035bd5b5d0a4008f7
swybean <swybean@gmail.com>
2024-09-20 14:03:13 +0900

    :recycle: Refactor: 적금 중도해지 페이지 탑바 적용
    <body>
    - Cancel_savings_@.tsx
    	- 통일된 탑바 적용

--- src/pages/savingsCancel/Cancel_savings_Password.tsx
+++ src/pages/savingsCancel/Cancel_savings_Password.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router';
 import LevelBar from '@/components/common/LevelBar';
-import XTopBar from '@/components/layouts/XTopbar';
 import PasswordInput from '@/components/common/PasswordInput';
+import TotalTopBar from '@/components/layouts/TotalTopBar';
 
 const CancelSavingsPassword = () => {
   const navigate = useNavigate();
@@ -11,10 +11,16 @@ const CancelSavingsPassword = () => {
   };
 
   return (
-    <div>
-      <XTopBar title='적금 중도해지' />
+    <div className='min-h-screen'>
+      <div className='fixed left-0 top-0 w-full'>
+        <TotalTopBar
+          title='적금 중도해지'
+          showBackButton={true}
+          showXButton={true}
+        />
+      </div>
 
-      <div className='mb-12 mt-4'>
+      <div className='mt-20'>
         <LevelBar currentLevel={2} totalLevel={2} />
       </div>
 
