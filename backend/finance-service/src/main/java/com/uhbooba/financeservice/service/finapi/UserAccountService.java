c45af7a44d003206879b4e3f9119af1ff0c47a1d
김인엽 <inyub98@gmail.com>
2024-09-20 17:53:39 +0900

    :sparkles: Feat: add user account logic for fin api

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/service/finapi/UserAccountService.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/service/finapi/UserAccountService.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -23,9 +24,41 @@ public class UserAccountService {
      *
      * @param userId user pk
      * @return
-     * @throws JsonProcessingException
+     * @throws
      */
-    public JsonNode getOrCreateUserAccount(Long userId) throws JsonProcessingException {
+    public Mono<JsonNode> getOrCreateUserAccount(Long userId) {
+        // 1. 요청 본문 생성
+        Map<String, Object> requestBody = new HashMap<>();
+        // 1.1 email 만들기
+        String email = generateUniqueEmail(userId);
+        requestBody.put("userId", email);
+
+        // 2. API 요청
+        HandlerParamWithoutHeader param = HandlerParamWithoutHeader.builder()
+                                                                   .url(baseUrl)
+                                                                   .requestBody(requestBody)
+                                                                   .build();
+
+        return finOpenApiHandler.apiRequest(param)
+                                .onErrorResume(Exception.class, e -> {
+                                    HandlerParamWithoutHeader searchParam = HandlerParamWithoutHeader.builder()
+                                                                                                     .url(
+                                                                                                         baseUrl
+                                                                                                             + "/search")
+                                                                                                     .requestBody(
+                                                                                                         requestBody)
+                                                                                                     .build();
+                                    return finOpenApiHandler.apiRequest(searchParam)
+                                                            .onErrorResume(Exception.class, ex -> {
+                                                                return Mono.error(
+                                                                    new FinOpenApiException(
+                                                                        "API 요청 실패: 계좌 검색, 이유: "
+                                                                            + ex.getMessage()));
+                                                            });
+                                });
+    }
+
+    public JsonNode getOrCreateUserAccountWithRT(Long userId) throws JsonProcessingException {
         // 1. 요청 본문 생성
         Map<String, Object> requestBody = new HashMap<>();
         // 1.1 email 만들기
@@ -40,14 +73,14 @@ public class UserAccountService {
                                              .url(baseUrl)
                                              .requestBody(requestBody)
                                              .build();
-            jsonNode = finOpenApiHandler.apiRequest(param);
+            jsonNode = finOpenApiHandler.apiRequestRT(param);
         } catch(HttpClientErrorException e) {
             param = HandlerParamWithoutHeader.builder()
                                              .url(baseUrl + "/search")
                                              .requestBody(requestBody)
                                              .build();
             // API 에서 4xx 에러가 발생한 경우 예외 처리(400 에러로 들어옴)
-            jsonNode = finOpenApiHandler.apiRequest(param);
+            jsonNode = finOpenApiHandler.apiRequestRT(param);
         } catch(Exception e) {
             // 일반적인 예외 처리
             throw new FinOpenApiException(e.getMessage());
