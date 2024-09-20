aa43ef1a5acd71d0798f027ca638eda0bbc259e5
q0_u_0p <q0_u_0p@naver.com>
2024-09-20 16:18:50 +0900

    ✨ feat : implement video API

--- backend/external-service/app/main.py
+++ backend/external-service/app/main.py
@@ -1,30 +1,37 @@
-# import sys
 # import os
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 #####################################################################################
-from .config.database import SessionLocal, engine, Base
-from .eureka_register import register_with_eureka
+import sys
+
 from fastapi import FastAPI
+
+from .config.database import SessionLocal
 from .config.database import engine, Base
-from .controllers import quiz, tts
+from .controllers import quiz, tts, video
+from .eureka_register import register_with_eureka
+
 #####################################################################################c
 Base.metadata.create_all(bind=engine)
 app = FastAPI(
     openapi_url="/external-service/v3/api-docs",  # OpenAPI 문서 경로 설정
-    docs_url="/external-service/docs",            # Swagger UI 경로 설정
+    docs_url="/external-service/docs",  # Swagger UI 경로 설정
 )
 #####################################################################################c
 app.include_router(quiz.router)
 app.include_router(tts.router)
+app.include_router(video.router)
 
 
 @app.get("/health-check")
 def read_root():
     return {"Hello": "World"}
+
+
 #####################################################################################c
 @app.on_event("startup")
 def startup_event():
     register_with_eureka()
+
 
 # Dependency
 def get_db():
@@ -34,16 +41,18 @@ def get_db():
     finally:
         db.close()
 
+
 #####################################################################################c
 import signal
 import uvicorn
 
 
 def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
+    print("You pressed Ctrl+C!")
     sys.exit(0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
     config = uvicorn.Config("main:app", port=8000, log_level="info", reload=True)
