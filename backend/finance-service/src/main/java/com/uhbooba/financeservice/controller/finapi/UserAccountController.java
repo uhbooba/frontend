b51f1cf18f5417a2bc300d6aa400079ef2406364
김인엽 <inyub98@gmail.com>
2024-09-23 16:21:31 +0900

    :sparkles: Feat: Controller 분리

---
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/controller/finapi/UserAccountController.java
@@ -0,0 +1,27 @@
+package com.uhbooba.financeservice.controller.finapi;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.uhbooba.financeservice.service.finapi.UserAccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/fin-api/user")
+public class UserAccountController {
+
+    private final UserAccountService userAccountService;
+
+    @GetMapping("/{userId}")
+    @Operation(summary = "사용자 계정 조회")
+    public Mono<JsonNode> getUserAccount(@PathVariable("userId") Long userId) {
+        return userAccountService.getOrCreateUserAccount(userId);
+    }
+}
