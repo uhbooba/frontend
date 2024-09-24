672de205d50440ccacd0c77ca0cf5050bf568c0f
swybean <swybean@gmail.com>
2024-09-24 17:23:41 +0900

    :recycle: Refactor: 탑바 및 메인페이지 수정
    <body>
    - TopBar.tsx
    	- 메인페이지에서 사용할 탑바 추가
    - Main.tsx
    	- 메인페이지 기본 골격 제작

--- frontend/src/components/layouts/TopBar.tsx
+++ frontend/src/components/layouts/TopBar.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import BackButton from '@/components/common/buttons/BackButton';
 import XButton from '@/components/common/buttons/XButton';
 import { useNavigate } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+import { TbMessageChatbot } from 'react-icons/tb';
 
 type TopBarProps = {
   title: string;
   showBackButton?: boolean;
   showXButton?: boolean;
+  showMainButton?: boolean;
   onXButtonClick?: () => void;
 };
 
@@ -14,6 +17,7 @@ const TopBar: React.FC<TopBarProps> = ({
   title,
   showBackButton = true,
   showXButton = true,
+  showMainButton = false,
   onXButtonClick,
 }) => {
   const navigate = useNavigate();
@@ -35,6 +39,18 @@ const TopBar: React.FC<TopBarProps> = ({
       {showXButton && (
         <div className='absolute right-0'>
           <XButton className='mr-4' onClick={onXButtonClick || handleGoHome} />
+        </div>
+      )}
+
+      {showMainButton && (
+        <div>
+          <div className=''>어부바</div>
+          <div className=''>
+            <TbMessageChatbot onClick={handleGoHome} />
+          </div>
+          <div className=''>
+            <FaUser onClick={handleGoHome} />
+          </div>
         </div>
       )}
     </div>
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -5,32 +5,60 @@ import TopBar from '@/components/layouts/TopBar';
 
 const ButtonConfig: ButtonConfigType[] = [
   {
-    label: '예금 가입',
+    label: '예금 가입 (계좌목록)',
     route: '/deposit',
     size: 'small',
     color: 'orange',
-    className: 'flex-grow',
+    className: 'flex-grow h-20',
   },
   {
-    label: '적금 가입',
+    label: '적금 가입 (계좌개설)',
     route: '/savings',
     size: 'small',
     color: 'orange',
-    className: 'flex-grow',
+    className: 'flex-grow h-20',
   },
   {
-    label: '예금 중도 해지',
+    label: '예금 중도 해지 (예적금)',
     route: '/cancel/deposit/explain',
     size: 'small',
     color: 'orange',
-    className: 'flex-grow',
+    className: 'flex-grow h-20',
   },
   {
-    label: '적금 중도 해지',
+    label: '적금 중도 해지 (환전)',
     route: '/cancel/savings/explain',
     size: 'small',
     color: 'orange',
-    className: 'flex-grow',
+    className: 'flex-grow h-20',
+  },
+  {
+    label: '공과금',
+    route: '/cancel/savings/explain',
+    size: 'small',
+    color: 'orange',
+    className: 'flex-grow h-20',
+  },
+  {
+    label: '금융사기',
+    route: '/cancel/savings/explain',
+    size: 'small',
+    color: 'orange',
+    className: 'flex-grow h-20',
+  },
+  {
+    label: '계좌입금(시드머니충전)',
+    route: '/cancel/savings/explain',
+    size: 'small',
+    color: 'orange',
+    className: 'flex-grow h-20',
+  },
+  {
+    label: '스탬프',
+    route: '/cancel/savings/explain',
+    size: 'small',
+    color: 'orange',
+    className: 'flex-grow h-20',
   },
 ];
 
@@ -43,9 +71,33 @@ const Main = () => {
 
   return (
     <div>
-      <TopBar title='메인페이지' showBackButton={false} showXButton={false} />
+      <TopBar
+        title=''
+        showBackButton={false}
+        showXButton={false}
+        showMainButton={true}
+      />
 
-      <div className='space-y-4 p-4'>
+      <div className='m-4 mt-8 h-40 rounded-lg bg-primary'>
+        <div className='p-4 pb-0 text-xl font-bold'>
+          자유입출금 계좌 111-222-3333
+        </div>
+        <div className='p-4 text-lg font-bold'>10,000,000원</div>
+        <div className='flex justify-around'>
+          <Button
+            label='돈 보내기'
+            size='small'
+            className='flex h-10 w-40 items-center justify-center bg-white'
+          />
+          <Button
+            label='계좌 조회(내역)'
+            size='small'
+            className='flex h-10 w-40 items-center justify-center bg-white'
+          />
+        </div>
+      </div>
+
+      <div className='grid grid-cols-2 gap-4 p-4'>
         {ButtonConfig.map((button, index) => (
           <Button
             key={index}
