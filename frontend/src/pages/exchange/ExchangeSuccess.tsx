3b2762ac2679646f8ca49ad9c6b132fd75880ba4
김지윤 <yunva17@naver.com>
2024-09-24 14:50:52 +0900

    ✨ Feat: 미션 성공 모달 추가

--- frontend/src/pages/exchange/ExchangeSuccess.tsx
+++ frontend/src/pages/exchange/ExchangeSuccess.tsx
@@ -1,13 +1,31 @@
 import Button from '@/components/common/buttons/Button';
 import TextBubble from '@/components/common/TextBubble';
 import TopBar from '@/components/layouts/TopBar';
+import MissionSuccessModal from '@/components/modals/MissionSuccessModal';
+import { useEffect, useState } from 'react';
 
 const ExchangeSuccess = () => {
+  const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSuccessModalOpen(true);
+    }, 1000); // 1초 뒤 미션성공 뜨도록(미션 성공한 사람의 경우만 뜨도록 추후 수정)
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
       <div className='fixed left-0 top-0 z-10 w-full'>
         <TopBar title='환전' showBackButton={false} />
       </div>
+      {isSuccessModalOpen && (
+        <MissionSuccessModal
+          name='환전'
+          onConfirm={() => setIsSuccessModalOpen(false)}
+        />
+      )}
 
       {/* 배경 이미지 설정 */}
       <div
