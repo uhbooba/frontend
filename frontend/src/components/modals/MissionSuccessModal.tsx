e7f6a1491f060ba56068e9f919ff90799c9b3538
김지윤 <yunva17@naver.com>
2024-09-24 15:08:13 +0900

    💄 Design: 미션 성공 컴포넌트 디자인 수정

--- frontend/src/components/modals/MissionSuccessModal.tsx
+++ frontend/src/components/modals/MissionSuccessModal.tsx
@@ -11,18 +11,21 @@ const MissionSuccessModal: React.FC<MissionSuccessModalProps> = ({
 }) => {
   return (
     <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
-      <div className='flex h-2/3 w-10/12 flex-col items-center rounded-xl bg-white text-center text-xl'>
-        <div className='mt-8'>
-          <p>{name} 미션에 성공하셨습니다.</p>
+      <div className='flex w-10/12 max-w-md flex-col items-center rounded-xl bg-white text-center text-lg'>
+        <div className='px-6 py-8'>
+          <p>{name ? `${name} ` : ''}미션에 성공하셨습니다.</p>
           <p>다음 미션에 도전해보세요!</p>
-          <img src='/assets/images/mission_stamp.png' className='w-72 py-10' />
+          <img
+            src='/assets/images/mission_stamp.png'
+            className='mx-auto my-8 w-60'
+            alt='미션 성공 스탬프'
+          />
         </div>
-
-        <div className='mb-8 flex h-full w-full items-end justify-center border-t-2 border-dashed'>
+        <div className='w-full border-t border-dashed py-8'>
           <Button
             label='확인'
             color='green'
-            className='h-fit w-2/3'
+            className='w-2/3 py-3'
             onClick={onConfirm}
           />
         </div>
