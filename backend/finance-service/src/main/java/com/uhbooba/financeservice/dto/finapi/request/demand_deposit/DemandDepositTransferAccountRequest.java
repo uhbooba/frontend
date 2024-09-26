66d09c0b9ef8018c3275052417df40d797f47d39
김인엽 <inyub98@gmail.com>
2024-09-26 13:27:10 +0900

    :sparkles: Feat : dto notBlank -> notnull

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/dto/finapi/request/demand_deposit/DemandDepositTransferAccountRequest.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/dto/finapi/request/demand_deposit/DemandDepositTransferAccountRequest.java
@@ -1,6 +1,7 @@
 package com.uhbooba.financeservice.dto.finapi.request.demand_deposit;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -8,7 +9,7 @@ import lombok.Builder;
 public record DemandDepositTransferAccountRequest(
     @NotBlank(message = "입금 계좌번호는 필수입니다.") @Size(max = 16, message = "계좌번호는 16글자입니다.") String depositAccountNo,
     @Size(max = 255, message = "최대 255자입니다.") String depositTransactionSummary,
-    @NotBlank(message = "거래금액은 필수입니다.") Long transactionBalance,
+    @NotNull(message = "거래금액은 필수입니다.") Long transactionBalance,
     @NotBlank(message = "출금 계좌번호는 필수입니다.") @Size(max = 16, message = "계좌번호는 16글자입니다.") String withdrawalAccountNo,
     @Size(max = 255, message = "최대 255자입니다.") String withdrawalTransactionSummary
 ) {
