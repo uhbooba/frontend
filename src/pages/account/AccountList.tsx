9c5b1a49ac3a98b5e1e6e06594507aaa19ab1b2b
이찬규 <isso8336@gmail.com>
2024-09-26 09:17:54 +0900

    :sparkles: Feat: AccountList TopBar BottomBar
    <body>
    - AccountList.tsx
    	- to add Topbar&BottomTab
    
    <footer>

---
+++ src/pages/account/AccountList.tsx
@@ -0,0 +1,14 @@
+import TopBar from "@/components/layouts/TopBar";
+import { BottomTab } from "@/components/layouts/BottomTab";
+
+
+const AccountList = () => {
+    <div>
+        <TopBar title="계좌 목록" />
+        <div className='fixed bottom-0 left-0 w-full'>
+            <BottomTab />
+        </div>
+    </div>
+};
+
+export default AccountList;
