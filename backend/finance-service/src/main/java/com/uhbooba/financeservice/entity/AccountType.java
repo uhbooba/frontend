9d951d48fff974e3a5c3de740595b01174f0216a
김인엽 <inyub98@gmail.com>
2024-09-26 13:20:20 +0900

    :sparkles: Feat : update account type

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/entity/AccountType.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/entity/AccountType.java
@@ -1,27 +1,34 @@
 package com.uhbooba.financeservice.entity;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public enum AccountType {
-    DEMAND_DEPOSIT(1),  // 수시입출금
-    FIXED_DEPOSIT(2),   // 정기예금
-    INSTALLMENT_SAVING(3),  // 정기적금
-    LOAN(4);  // 대출
+    DEMAND_DEPOSIT("1"),  // 수시입출금
+    FIXED_DEPOSIT("2"),   // 정기예금
+    INSTALLMENT_SAVING("3"),  // 정기적금
+    LOAN("4");  // 대출
 
-    private final int code;
+    private final String code;
 
-    AccountType(int code) {
+    AccountType(String code) {
         this.code = code;
     }
 
-    public int getCode() {
+    public String getCode() {
         return code;
     }
 
-    public static AccountType fromCode(int code) {
+    public static AccountType fromCode(String code) {
+        log.info("이거 받음: " + code);
         for(AccountType type : AccountType.values()) {
-            if(type.getCode() == code) {
+            if(type.getCode()
+                   .equals(code)) {
                 return type;
             }
         }
+        // 디버그 로그 추가
+        log.info("Invalid account type code received: " + code);
         throw new IllegalArgumentException("Invalid account type code: " + code);
     }
 }
