42489e2869eacdfc3068faa2a22f7498d5abd1db
김인엽 <inyub98@gmail.com>
2024-09-26 13:20:04 +0900

    :sparkles: Feat : add account repository

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/repository/AccountRepository.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.uhbooba.financeservice.repository;
 import com.uhbooba.financeservice.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    
+public interface AccountRepository extends JpaRepository<Account, Integer>,
+    AccountCustomRepository {
+
 }
