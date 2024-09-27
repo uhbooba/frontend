d42db76be504c841463475f3df4908107950997a
이찬규 <isso8336@gmail.com>
2024-09-27 17:24:34 +0900

    :sparkles: Feat: AccountProducts Init skeleton complete
    <body>
    - AccountDepositProduct.tsx
    	- to change title of interstRate according to selectedDepositProducts
    - AccountDepositProducts.tsx
    	- to complete whole page of AccountDepositProducts
    
    <footer>

--- frontend/src/components/common/AccountDepositProduct.tsx
+++ frontend/src/components/common/AccountDepositProduct.tsx
@@ -5,26 +5,33 @@ type AccountDepositProductProps = {
     name: string;
     interestRate: number ;
     minimumAmount: number;
-    minimumPeriod: number
+    minimumPeriod: number;
+    moveTo: string;
+    selectedProduct: string;
 };
 
-const AccountDepositProduct: React.FC<AccountDepositProductProps> = ({ name, interestRate, minimumAmount, minimumPeriod }) => {
+const AccountDepositProduct: React.FC<AccountDepositProductProps> = ({ name, interestRate, minimumAmount, minimumPeriod, moveTo, selectedProduct }) => {
     const navigate = useNavigate();
 
     return(
         <div 
             className='w-[360px] h-[148px] bg-[#FFD362] rounded-xl mt-[20px] text-center'
+            onClick={() => navigate(moveTo)}
             >
             <div className='flex m-6 justify-between '>
-                <div className='flex flex-col w-[104px] h-[104px] bg-[#FFAF2A] rounded-xl text-center text-2xl font-extrabold justify-center'>
+                <div className='flex flex-col mt-6 w-[104px] h-[104px] bg-[#FFAF2A] rounded-xl text-center text-2xl font-extrabold justify-center'>
                     {name}
                 </div>
-                <div className='flex flex-col my-2 justify-between font-extrabold'>
-                    <div>이자율</div>
+                <div className='flex flex-col mt-8 my-2 justify-between font-extrabold'>
+                    {selectedProduct === '예금 상품' ? (
+                        <div>연 이자율</div>
+                    ) : (
+                        <div>이자율</div>
+                    )}
                     <div>최소금액</div>
                     <div>최소기간</div>
                 </div>
-                <div className='flex flex-col my-2 justify-between'>
+                <div className='flex flex-col mt-8 my-2 justify-between'>
                     <div>{ interestRate }%</div>
                     <div>{ minimumAmount/10000 }만 원</div>
                     <div>{ minimumPeriod }개월</div>
---
+++ frontend/src/pages/account/AccountDepositProducts.tsx
@@ -0,0 +1,135 @@
+import KeywordButtons from "@/components/common/KeywordButtons";
+import TopBar from "@/components/layouts/TopBar";
+import { BottomTab } from "@/components/layouts/BottomTab";
+// import { useNavigate } from "react-router";
+import { useState } from "react";
+import AccountDepositProduct from "@/components/common/AccountDepositProduct";
+
+
+const AccountDepositProducts = () => {
+    const [selectedKeyword, setSelectedKeyword] = useState('예금 상품');
+
+    // const navigate = useNavigate();
+
+    const keywordClick = (keyword: string) => {
+        setSelectedKeyword(keyword);
+    };
+
+    const keywords = ['예금 상품', '적금 상품']
+
+    const DepositProducts = [
+        {
+            name: '정기예금 1번 상품',
+            interestRate: 7,
+            minimumAmmount: 100000,
+            minimumPeriod: 6,
+            moveTo: '',
+            selectedProduct: '예금 상품'
+        },
+        {
+            name: '정기예금 2번 상품',
+            interestRate: 10,
+            minimumAmmount: 500000,
+            minimumPeriod: 12,
+            moveTo: '',
+            selectedProduct: '예금 상품'
+        },
+        {
+            name: '정기예금 3번 상품',
+            interestRate: 12,
+            minimumAmmount: 1000000,
+            minimumPeriod: 24,
+            moveTo: '',
+            selectedProduct: '예금 상품'
+        },
+    ]
+    const SavingsProducts = [
+        {
+            name: '정기적금 1번 상품',
+            interestRate: 5,
+            minimumAmmount: 10000,
+            minimumPeriod: 6,
+            moveTo: '',
+            selectedProduct: '적금 상품'
+        },
+        {
+            name: '정기적금 2번 상품',
+            interestRate: 7,
+            minimumAmmount: 100000,
+            minimumPeriod: 12,
+            moveTo: '',
+            selectedProduct: '적금 상품'            
+        },
+        {
+            name: '정기적금 3번 상품',
+            interestRate: 10,
+            minimumAmmount: 500000,
+            minimumPeriod: 24,
+            moveTo: '',
+            selectedProduct: '적금 상품'            
+        },
+    ]
+    
+    return(
+    <div>
+        <TopBar 
+            title='계좌 개설' 
+            showXButton={false}
+        />
+
+        <div className='mt-4 border-b-2'>
+            <KeywordButtons
+            keywords={keywords}
+            onKeywordClick={keywordClick}
+            keywordBtnColor={selectedKeyword}
+            />
+        </div>
+
+        <div className="flex flex-col justify-center items-center" >
+            {selectedKeyword === '예금 상품' ? (
+                <div>
+                    {DepositProducts.map((product, index) => (
+                        <AccountDepositProduct 
+                            key={index}
+                            name={product.name}
+                            interestRate={product.interestRate}
+                            minimumAmount={product.minimumAmmount}
+                            minimumPeriod={product.minimumPeriod}
+                            moveTo={product.moveTo}
+                            selectedProduct={product.selectedProduct}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <div>
+                    {SavingsProducts.map((product, index) => (
+                        <AccountDepositProduct 
+                            key={index}
+                            name={product.name}
+                            interestRate={product.interestRate}
+                            minimumAmount={product.minimumAmmount}
+                            minimumPeriod={product.minimumPeriod}
+                            moveTo={product.moveTo}
+                            selectedProduct={product.selectedProduct}
+                        />
+                    ))}
+                </div>
+                )
+            };
+            {/* {ProductsList.map((product, index) => (
+                <AccountProduct 
+                    key={index}
+                    name={product.name}
+                    description={product.description}
+                    moveTo={product.moveTo}
+                />                
+            ))} */}
+=        </div>
+        <div className='fixed bottom-0 left-0 w-full'>
+            <BottomTab />
+        </div>
+    </div>
+    );
+};
+
+export default AccountDepositProducts;
