917fd43ae9e2696e9543f9a93e4f708a8002b0b8
이찬규 <isso8336@gmail.com>
2024-09-27 17:43:04 +0900

    :art: Style: code prettier apply
    <body>
    - AccountDepositProduct.tsx
    	- code prettier apply
    - AccountProduct.tsx
    	- code prettier apply
    - AccountDepositProducts.tsx
    	- code prettier apply
    - AccountProductsList.tsx
    	- code prettier apply
    - router.tsx
    	- code prettier apply
    
    <footer>

--- src/components/common/AccountDepositProduct.tsx
+++ src/components/common/AccountDepositProduct.tsx
@@ -1,48 +1,55 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router';
 
 type AccountDepositProductProps = {
-    name: string;
-    interestRate: number ;
-    minimumAmount: number;
-    minimumPeriod: number;
-    moveTo: string;
-    selectedProduct: string;
+  name: string;
+  interestRate: number;
+  minimumAmount: number;
+  minimumPeriod: number;
+  moveTo: string;
+  selectedProduct: string;
 };
 
-const AccountDepositProduct: React.FC<AccountDepositProductProps> = ({ name, interestRate, minimumAmount, minimumPeriod, moveTo, selectedProduct }) => {
-    const navigate = useNavigate();
+const AccountDepositProduct: React.FC<AccountDepositProductProps> = ({
+  name,
+  interestRate,
+  minimumAmount,
+  minimumPeriod,
+  moveTo,
+  selectedProduct,
+}) => {
+  const navigate = useNavigate();
 
-    return(
-        <div 
-            className='w-[360px] h-[148px] bg-[#FFD362] rounded-xl mt-[20px] text-center'
-            onClick={() => navigate(moveTo)}
-            >
-            <div className='flex m-6 justify-between '>
-                <div className='flex flex-col mt-6 w-[104px] h-[104px] bg-[#FFAF2A] rounded-xl text-center text-2xl font-extrabold justify-center'>
-                    {name}
-                </div>
-                <div className='flex flex-col mt-8 my-2 justify-between font-extrabold'>
-                    {selectedProduct === '예금 상품' ? (
-                        <div>연 이자율</div>
-                    ) : (
-                        <div>이자율</div>
-                    )}
-                    <div>최소금액</div>
-                    <div>최소기간</div>
-                </div>
-                <div className='flex flex-col mt-8 my-2 justify-between'>
-                    <div>{ interestRate }%</div>
-                    <div>{ minimumAmount/10000 }만 원</div>
-                    <div>{ minimumPeriod }개월</div>
-                </div>
-            </div>
-            {/* <div className='mt-6 mb-[5px] font-bold'>
+  return (
+    <div
+      className='mt-[20px] h-[148px] w-[360px] rounded-xl bg-[#FFD362] text-center'
+      onClick={() => navigate(moveTo)}
+    >
+      <div className='m-6 flex justify-between'>
+        <div className='mt-6 flex h-[104px] w-[104px] flex-col justify-center rounded-xl bg-[#FFAF2A] text-center text-2xl font-extrabold'>
+          {name}
+        </div>
+        <div className='my-2 mt-8 flex flex-col justify-between font-extrabold'>
+          {selectedProduct === '예금 상품' ? (
+            <div>연 이자율</div>
+          ) : (
+            <div>이자율</div>
+          )}
+          <div>최소금액</div>
+          <div>최소기간</div>
+        </div>
+        <div className='my-2 mt-8 flex flex-col justify-between'>
+          <div>{interestRate}%</div>
+          <div>{minimumAmount / 10000}만 원</div>
+          <div>{minimumPeriod}개월</div>
+        </div>
+      </div>
+      {/* <div className='mt-6 mb-[5px] font-bold'>
                 <div className='text-[24px] text-[#5A6A59]'>t</div>
                 <div className='mt-6 text-[18px]'>f</div>
             </div> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default AccountDepositProduct;
