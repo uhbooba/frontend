b83af82ad0f0b47eec12b92f2d20f939278d0e07
김인엽 <inyub98@gmail.com>
2024-09-27 16:03:49 +0900

    :sparkles: Feat : change notblank to notnull for long type

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/dto/finapi/request/deposit/DepositAccountCreateRequest.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/dto/finapi/request/deposit/DepositAccountCreateRequest.java
@@ -1,6 +1,7 @@
 package com.uhbooba.financeservice.dto.finapi.request.deposit;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -8,7 +9,7 @@ import lombok.Builder;
 public record DepositAccountCreateRequest(
     @NotBlank(message = "출금할 수시입출금 계좌번호를 입력하세요") @Size(max = 20) String withdrawalAccountNo,
     @NotBlank(message = "가입할 예금 상품고유번호를 입력하세요") @Size(max = 20) String accountTypeUniqueNo,
-    @NotBlank Long depositBalance
+    @NotNull Long depositBalance
 ) {
 
 }
