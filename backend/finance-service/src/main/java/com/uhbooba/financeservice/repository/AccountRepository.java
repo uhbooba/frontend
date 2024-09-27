8dae8d3fa13dc548fc39d07dbf670d7d7618355c
김인엽 <inyub98@gmail.com>
2024-09-27 16:03:08 +0900

    :sparkles: Feat : add method to account repository

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/repository/AccountRepository.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/repository/AccountRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Integer>,
     AccountCustomRepository {
 
+    Account findByAccountNo(String accountNo);
 }
