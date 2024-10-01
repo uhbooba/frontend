0803afa9b3de8ba69206c611596dd0038a6aa7e7
swybean <swybean@gmail.com>
2024-10-01 20:06:50 +0900

    :sparkles: Feat: 예금 api 설정
    <body>
    - deposit.ts
    	- 예금 관련 axios 모음
    - CancelDepositProduct.tsx
    	- 예금 중도해지 api 적용- 미완성

---
+++ src/services/deposit.ts
@@ -0,0 +1,43 @@
+import { axiosInstance } from '@/utils/axiosInstance';
+
+// 예금 상품 전체 조회
+export const getDepositProducts = () => {
+  const response = axiosInstance.get('/finances/deposits/products');
+  return response;
+};
+
+// 사용자의 예금 계좌 목록 조회
+export const getUserDepositAccounts = (userId: number) => {
+  const response = axiosInstance.get('/finances/deposits/accounts', {
+    params: { userId },
+  });
+  return response;
+};
+
+// 예금 만기 이자 조회
+export const getDepositExpiryInterests = (
+  userId: number,
+  accountNo: string,
+) => {
+  const response = axiosInstance.get(
+    '/finances/deposits/accounts/expiry-interests',
+    {
+      params: { userId, accountNo },
+    },
+  );
+  return response;
+};
+
+// 예금 중도 해지 시 이자 조회
+export const getEarlyTerminationInterest = (
+  userId: number,
+  accountNo: string,
+) => {
+  const response = axiosInstance.get(
+    '/finances/deposits/accounts/early-termination-interest',
+    {
+      params: { userId, accountNo },
+    },
+  );
+  return response;
+};
