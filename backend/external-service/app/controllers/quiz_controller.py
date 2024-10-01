8e65e1d1c2cae1ecf7a53c7e9e7b4a64418c6773
q0_u_0p <q0_u_0p@naver.com>
2024-10-02 01:01:42 +0900

    ✨ Feat : add quiz data and modify API

--- backend/external-service/app/controllers/quiz_controller.py
+++ backend/external-service/app/controllers/quiz_controller.py
@@ -18,8 +18,8 @@ router = APIRouter(prefix="/quiz", tags=["quiz"])
 @router.get("", response_model=ok_res)
 def get_all_quiz(db: Session = Depends(get_db)):
     try:
-        quizzes = QuizService.get_all_quizzes(db)
-        return ok_res(data=quizzes)
+        data = QuizService.get_all_quizzes(db)
+        return ok_res(data=data)
     except Exception as e:
         error_response = {"status": "error", "data": f"Failed to get quizzes: {str(e)}"}
         return JSONResponse(status_code=500, content=error_response)
@@ -27,11 +27,10 @@ def get_all_quiz(db: Session = Depends(get_db)):
 
 @router.get("/{part}", response_model=ok_res)
 def get_quiz(part: int, db: Session = Depends(get_db)):
-    response = QuizService.get_quizzes_by_part(part, db)
-    if not response.data:
-        error_response = {
-            "status": "not found",
-            "data": f"No quizzes found for part {part}",
-        }
-        return JSONResponse(status_code=404, content=error_response)
-    return ok_res(data=response)
+    try:
+        data = QuizService.get_quizzes_by_part(part, db)
+        return ok_res(data=data)
+    except Exception as e:
+        return JSONResponse(
+            status_code=404, content={"status": "error", "message": str(e)}
+        )
--- backend/external-service/app/services/quiz_service.py
+++ backend/external-service/app/services/quiz_service.py
@@ -1,37 +1,36 @@
 from itertools import groupby
-from operator import attrgetter
 
 from sqlalchemy.orm import Session
 
 from ..models.quiz_model import Quiz
-from ..schemas.quiz_schema import QuizItem, QuizPartResponse, QuizResponse
+from ..schemas.quiz_schema import QuizItem, QuizResponse
 
 
 class QuizService:
+    @staticmethod
+    def quiz_to_quiz_item(quiz: Quiz) -> QuizItem:
+        return QuizItem(
+            number=quiz.number,
+            question=quiz.question,
+            answer="O" if quiz.answer else "X",
+            comment=quiz.comment,
+        )
 
     # 모든 퀴즈를 가져오는 함수
     @staticmethod
     def get_all_quizzes(db: Session):
         quizzes = db.query(Quiz).all()
-        quiz_items = [
-            QuizItem(
-                part=quiz.part,
-                number=quiz.number,
-                question=quiz.question,
-                answer="O" if quiz.answer else "X",
-                comment=quiz.comment,
-            )
-            for quiz in quizzes
-        ]
 
-        # part로 그룹화
-        grouped_quizzes = groupby(
-            sorted(quiz_items, key=attrgetter("part")), key=attrgetter("part")
-        )
+        # part와 topic으로 그룹화
+        grouped_quizzes = groupby(quizzes, key=lambda x: (x.part, x.topic))
 
         quizzes_by_part = [
-            QuizPartResponse(part=part, quizzes=list(items))
-            for part, items in grouped_quizzes
+            QuizResponse(
+                part=part,
+                topic=topic,
+                quizzes=[QuizService.quiz_to_quiz_item(quiz) for quiz in items],
+            )
+            for (part, topic), items in grouped_quizzes
         ]
 
         return quizzes_by_part
@@ -41,15 +40,11 @@ class QuizService:
     def get_quizzes_by_part(part: int, db: Session):
         quizzes = db.query(Quiz).filter(Quiz.part == part).all()
 
-        quiz_items = [
-            QuizItem(
-                part=quiz.part,
-                number=quiz.number,
-                question=quiz.question,
-                answer="O" if quiz.answer else "X",
-                comment=quiz.comment,
-            )
-            for quiz in quizzes
-        ]
+        if not quizzes:
+            raise Exception(f"퀴즈를 찾을 수 없습니다: part is {part}")
 
-        return QuizResponse(status="success", data=quiz_items)
+        topic = quizzes[0].topic
+
+        quiz_items = [QuizService.quiz_to_quiz_item(quiz) for quiz in quizzes]
+
+        return QuizResponse(part=part, topic=topic, quizzes=quiz_items)
