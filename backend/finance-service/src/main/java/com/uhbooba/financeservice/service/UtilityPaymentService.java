a5df5d7dc751f2c6e6050f3c9c428cd86fe3f52c
김인엽 <inyub98@gmail.com>
2024-10-02 01:31:14 +0900

    :sparkles: Feat: add init setting service logic

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/service/UtilityPaymentService.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/service/UtilityPaymentService.java
@@ -3,9 +3,13 @@ package com.uhbooba.financeservice.service;
 import com.uhbooba.financeservice.dto.UtilityPaymentRequest;
 import com.uhbooba.financeservice.dto.finapi.request.demand_deposit.DemandDepositTransferAccountRequest;
 import com.uhbooba.financeservice.dto.finapi.response.demand_deposit.DemandDepositAccountResponse;
+import com.uhbooba.financeservice.entity.Account;
+import com.uhbooba.financeservice.entity.AccountType;
+import com.uhbooba.financeservice.entity.UserAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,8 @@ public class UtilityPaymentService {
     private String utilityPaymentAccountNo;
 
     private final DemandDepositService demandDepositService;
+    private final UserAccountService userAccountService;
+    private final AccountService accountService;
 
     public void payUtilities(
         Integer userId,
@@ -27,15 +33,33 @@ public class UtilityPaymentService {
                                                                                                  .depositAccountNo(
                                                                                                      utilityPaymentAccountNo)
                                                                                                  .depositTransactionSummary(
-                                                                                                     "10월 공과금 납부")
+                                                                                                     "공과금 납부")
                                                                                                  .transactionBalance(
                                                                                                      request.amount())
                                                                                                  .withdrawalAccountNo(
                                                                                                      demandDepositAccount.accountNo())
                                                                                                  .withdrawalTransactionSummary(
-                                                                                                     "10월 공과금 납부")
+                                                                                                     "공과금 납부")
 
                                                                                                  .build();
         demandDepositService.transferDemandDepositAccount(userId, transferRequest);
+    }
+
+    @Transactional
+    public void initValueForUtilities() {
+        // 1. user account 만들기
+        int userId = 1_000_000;
+        UserAccount userAccount = userAccountService.checkOrCreateUserAccount(userId);
+
+        // 2. account(입출금) 만들기
+        DemandDepositAccountResponse request = DemandDepositAccountResponse.builder()
+                                                                           .accountName("한국 전력 공사")
+                                                                           .accountTypeCode(
+                                                                               AccountType.DEMAND_DEPOSIT.getCode())
+                                                                           .username("한국 전력 공사")
+                                                                           .accountNo(
+                                                                               utilityPaymentAccountNo)
+                                                                           .build();
+        Account account = accountService.createAccount(request, userAccount);
     }
 }
