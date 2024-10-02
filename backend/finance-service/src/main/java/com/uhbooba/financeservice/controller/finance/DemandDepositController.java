3f199f71e727d1dff7d5bbeafab5814c3e85f50e
김인엽 <inyub98@gmail.com>
2024-10-02 17:42:41 +0900

    :sparkles: Feat: change logic to get data from header

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/controller/finance/DemandDepositController.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/controller/finance/DemandDepositController.java
@@ -5,19 +5,20 @@ import static com.uhbooba.financeservice.util.ApiDescriptions.Common.OUTPUT;
 import static com.uhbooba.financeservice.util.ApiDescriptions.DemandDepositController.DEMAND_DEPOSIT_ACCOUNT_RESPONSE;
 
 import com.uhbooba.financeservice.dto.CommonResponse;
+import com.uhbooba.financeservice.dto.UserHeaderInfo;
 import com.uhbooba.financeservice.dto.finapi.request.demand_deposit.DemandDepositCreateRequest;
 import com.uhbooba.financeservice.dto.finapi.request.demand_deposit.DemandDepositDepositAccountRequest;
 import com.uhbooba.financeservice.dto.finapi.request.demand_deposit.DemandDepositGetTransactionRequest;
 import com.uhbooba.financeservice.dto.finapi.request.demand_deposit.DemandDepositGetTransactionsRequest;
 import com.uhbooba.financeservice.dto.finapi.request.demand_deposit.DemandDepositTransferAccountRequest;
 import com.uhbooba.financeservice.dto.finapi.response.demand_deposit.DemandDepositAccountBalanceResponse;
-import com.uhbooba.financeservice.dto.finapi.response.demand_deposit.DemandDepositAccountHolderResponse;
 import com.uhbooba.financeservice.dto.finapi.response.demand_deposit.DemandDepositAccountResponse;
 import com.uhbooba.financeservice.dto.finapi.response.demand_deposit.DemandDepositDepositResponse;
 import com.uhbooba.financeservice.dto.finapi.response.demand_deposit.DemandDepositResponse;
 import com.uhbooba.financeservice.dto.finapi.response.demand_deposit.DemandDepositTransferResponse;
 import com.uhbooba.financeservice.dto.finapi.response.transaction.TransactionListResponse;
 import com.uhbooba.financeservice.dto.finapi.response.transaction.TransactionResponse;
+import com.uhbooba.financeservice.dto.response.AccountResponse;
 import com.uhbooba.financeservice.service.DemandDepositService;
 import com.uhbooba.financeservice.util.CommonUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -63,102 +64,106 @@ public class DemandDepositController {
     @PostMapping("/accounts")
     @Operation(summary = "[사용 X] 입출금 계좌 생성", description = "입출금 계좌를 생성합니다. \n 프론트에서 직접적인 호출 없이 User Service 와의 통신으로 자동 진행됩니다.")
     public CommonResponse<DemandDepositAccountResponse> createDemandDepositAccount(
-        //        @RequestParam("userId") Integer userId,
         @RequestHeader HttpHeaders headers
     ) {
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료", demandDepositService.createDemandDepositAccount(userId));
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료",
+                                 demandDepositService.createDemandDepositAccount(userHeaderInfo));
     }
 
     @GetMapping("/accounts/detail")
     @Operation(summary = "입출금계좌 조회", description = "입출금 계좌를 상세 조회 합니다." + INPUT + OUTPUT
         + DEMAND_DEPOSIT_ACCOUNT_RESPONSE)
-    public CommonResponse<DemandDepositAccountResponse> getAccountDetail(
-        //        @RequestParam("userId") Integer userId
+    public CommonResponse<AccountResponse> getAccountDetail(
         @RequestHeader HttpHeaders headers
     ) {
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료", demandDepositService.getDemandDepositAccount(userId));
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료",
+                                 demandDepositService.getDemandDepositAccount(userHeaderInfo));
     }
 
     @GetMapping("/accounts/holder")
     @Operation(summary = "입출금계좌 소유자 이름 조회", description = """
             해당 계좌의 소유자 정보를 확인합니다. \n
         """)
-    public CommonResponse<DemandDepositAccountHolderResponse> getAccountHolderName(
-        //        @RequestParam("userId") Integer userId,
+    public CommonResponse<AccountResponse> getAccountHolderName(
         @RequestHeader HttpHeaders headers,
         @RequestParam("accountNo") String accountNo
     ) {
         if(accountNo.length() > 16) {
             throw new IllegalArgumentException("계좌 번호는 16글자입니다.");
         }
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료",
-                                 demandDepositService.getDemandDepositAccountHolderName(userId,
-                                                                                        accountNo));
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료", demandDepositService.getDemandDepositAccountHolderName(
+            userHeaderInfo, accountNo));
     }
 
     @GetMapping("/accounts/balances")
     @Operation(summary = "입출금계좌 잔액 조회")
     public CommonResponse<DemandDepositAccountBalanceResponse> getAccountBalance(
-        //        @RequestParam("userId") Integer userId,
         @RequestHeader HttpHeaders headers,
         @RequestParam("accountNo") String accountNo
     ) {
         if(accountNo.length() > 16) {
             throw new IllegalArgumentException("계좌 번호는 16글자입니다.");
         }
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료", demandDepositService.getDemandDepositAccountBalance(userId,
-                                                                                           accountNo));
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료",
+                                 demandDepositService.getDemandDepositAccountBalance(userHeaderInfo,
+                                                                                     accountNo));
     }
 
     @PostMapping("/accounts/deposit")
     @Operation(summary = "입출금계좌에 예금")
     public CommonResponse<DemandDepositDepositResponse> depositAccount(
-        //        @RequestParam("userId") Integer userId,
         @RequestHeader HttpHeaders headers,
         @Valid @RequestBody DemandDepositDepositAccountRequest accountRequest
     ) {
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료", demandDepositService.depositDemandDepositAccount(userId,
-                                                                                        accountRequest));
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료",
+                                 demandDepositService.depositDemandDepositAccount(userHeaderInfo,
+                                                                                  accountRequest));
     }
 
     @PostMapping("/accounts/transfer")
     @Operation(summary = "입출금계좌에서 이체")
     public CommonResponse<List<DemandDepositTransferResponse>> transferAccount(
-        //        @RequestParam("userId") Integer userId,
         @RequestHeader HttpHeaders headers,
         @Valid @RequestBody DemandDepositTransferAccountRequest transferAccountRequest
     ) {
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료", demandDepositService.transferDemandDepositAccount(userId,
-                                                                                         transferAccountRequest));
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료",
+                                 demandDepositService.transferDemandDepositAccount(userHeaderInfo,
+                                                                                   transferAccountRequest));
     }
 
     @PostMapping("/transactions/histories")
     @Operation(summary = "[사용 X] 입출금계좌 거래내역 조회")
     public CommonResponse<TransactionListResponse> getTransactionHistories(
-        //        @RequestParam("userId") Integer userId,
         @RequestHeader HttpHeaders headers,
         @Valid @RequestBody DemandDepositGetTransactionsRequest getTransactionsRequest
     ) {
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료", demandDepositService.getTransactionHistories(userId,
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료", demandDepositService.getTransactionHistories(userHeaderInfo,
                                                                                     getTransactionsRequest));
     }
 
     @PostMapping("/transactions/histories/detail")
     @Operation(summary = "[사용 X] 입출금계좌 단일 거래내역 조회")
     public CommonResponse<TransactionResponse> getTransactionHistory(
-        //        @RequestParam("userId") Integer userId,
         @RequestHeader HttpHeaders headers,
         @Valid @RequestBody DemandDepositGetTransactionRequest getTransactionRequest
     ) {
-        Integer userId = CommonUtil.getMemberId(headers);
-        return CommonResponse.ok("완료", demandDepositService.getTransactionHistory(userId,
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+
+        return CommonResponse.ok("완료", demandDepositService.getTransactionHistory(userHeaderInfo,
                                                                                   getTransactionRequest));
     }
 }
