4610bb9a8b75e88699e19f9e84e49a8539a4275b
swybean <swybean@gmail.com>
2024-10-02 13:56:11 +0900

    :recycle: Refactor: 메인페이지 및 적금 중도해지 api
    <body>
    - Main.tsx
    	- 메인페이지 메인계좌 api 연결
    - CancelSavingsProduct.tsx
    	- 적금 중도해지 페이지 api 연결

--- src/pages/deposit/DepositMoney.tsx
+++ src/pages/deposit/DepositMoney.tsx
@@ -72,6 +72,7 @@ const DepositMoney = () => {
   // 얼마로 시작할까요? 버튼 클릭 함수
   const amountClick = (index: number, amount: string) => {
     setAmountBtnColor(amount);
+    console.log(index);
 
     if (amount !== '직접입력') {
       setSelectMoney(formatMoney(amount.replace('원', '')));
