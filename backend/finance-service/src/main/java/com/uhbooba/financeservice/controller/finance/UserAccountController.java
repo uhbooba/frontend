736901e54e1b9c3ffa56b60c8c2e6c77c7060251
김인엽 <inyub98@gmail.com>
2024-10-02 17:44:26 +0900

    :sparkles: Feat: change url and header extract logic

--- backend/finance-service/src/main/java/com/uhbooba/financeservice/controller/finance/UserAccountController.java
+++ backend/finance-service/src/main/java/com/uhbooba/financeservice/controller/finance/UserAccountController.java
@@ -1,12 +1,15 @@
 package com.uhbooba.financeservice.controller.finance;
 
+import com.uhbooba.financeservice.dto.UserHeaderInfo;
 import com.uhbooba.financeservice.service.UserAccountService;
+import com.uhbooba.financeservice.util.CommonUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +22,12 @@ public class UserAccountController {
 
     private final UserAccountService userAccountService;
 
-    @GetMapping("/{userId}")
+    @GetMapping("/account")
     @Operation(summary = "금융 API 사용자 계정 생성 또는 조회", description = "회원가입 시 여기로 요청 보내기")
-    public void getUserAccount(@PathVariable("userId") Integer userId) {
-        userAccountService.checkOrCreateUserAccount(userId);
+    public void getUserAccount(
+        @RequestHeader HttpHeaders headers
+    ) {
+        UserHeaderInfo userHeaderInfo = CommonUtil.getUserHeaderInfo(headers);
+        userAccountService.checkOrCreateUserAccount(userHeaderInfo);
     }
 }
