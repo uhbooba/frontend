ac3535e9b4430c0ad4fdcf04927a1fecb1f3ca08
이찬규 <isso8336@gmail.com>
2024-10-02 17:41:11 +0900

    :art: Style: ChatMessage style changed
    <body>
    - ChatMessage.tsx
    	- to change style of ChatMessage
    
    <footer>

--- frontend/src/components/chatbot/ChatMessage.tsx
+++ frontend/src/components/chatbot/ChatMessage.tsx
@@ -7,11 +7,11 @@ type ChatMessageProps = {
 
 const ChatMessage: React.FC<ChatMessageProps> = ({ question, answer }) => {
   return (
-    <div className='mt-16 mb-4'>
+    <div className='mb-4 mt-16'>
       {/* 질문을 오른쪽 정렬 */}
       {question && (
-        <div className='flex justify-end mb-2'>
-          <div className='w-auto max-w-[70%] whitespace-pre-wrap rounded-lg bg-blue-500 p-2 text-left text-white'>
+        <div className='mb-2 flex justify-end'>
+          <div className='w-auto max-w-[70%] whitespace-pre-wrap rounded-lg bg-primary p-2 text-left text-black'>
             <p>{question}</p>
           </div>
         </div>
