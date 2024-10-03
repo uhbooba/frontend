c929deb1a80730c0cbe5558b8e90a0596de2d268
swybean <swybean@gmail.com>
2024-10-04 05:21:38 +0900

    axios 함수들 async 적용

--- frontend/src/services/deposit.ts
+++ frontend/src/services/deposit.ts
@@ -1,25 +1,25 @@
 import { axiosInstance } from '@/utils/axiosInstance';
 
 // 예금 상품 전체 조회
-export const getDepositProducts = () => {
-  const response = axiosInstance('/finances/deposits/products');
+export const getDepositProducts = async () => {
+  const response = await axiosInstance('/finances/deposits/products');
   return response;
 };
 
 // 사용자의 예금 계좌 목록 조회
-export const getUserDepositAccounts = (userId: number) => {
-  const response = axiosInstance('/finances/deposits/accounts', {
+export const getUserDepositAccounts = async (userId: number) => {
+  const response = await axiosInstance('/finances/deposits/accounts', {
     params: { userId },
   });
   return response;
 };
 
 // 예금 만기 이자 조회
-export const getDepositExpiryInterests = (
+export const getDepositExpiryInterests = async (
   userId: number,
   accountNo: string,
 ) => {
-  const response = axiosInstance(
+  const response = await axiosInstance(
     '/finances/deposits/accounts/expiry-interests',
     {
       params: { userId, accountNo },
@@ -29,11 +29,11 @@ export const getDepositExpiryInterests = (
 };
 
 // 예금 중도 해지 시 이자 조회
-export const getEarlyTerminationInterest = (
+export const getEarlyTerminationInterest = async (
   userId: number,
   accountNo: string,
 ) => {
-  const response = axiosInstance(
+  const response = await axiosInstance(
     '/finances/deposits/accounts/early-termination-interest',
     {
       params: { userId, accountNo },
