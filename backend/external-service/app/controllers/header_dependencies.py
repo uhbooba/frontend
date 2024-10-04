66a93c424ee15254ca836bfa7c85f8b3730b7b3f
q0_u_0p <q0_u_0p@naver.com>
2024-10-04 17:43:46 +0900

    🐛 Fix: request header reference

--- backend/external-service/app/controllers/header_dependencies.py
+++ backend/external-service/app/controllers/header_dependencies.py
@@ -12,14 +12,11 @@ class UserHeaderInfo:
 
 def get_user_header_info(
         request: Request,
-        # x_userid: Optional[str] = Header(None),
-        # x_name: Optional[str] = Header(None),
-        # headers: Header(None)
 ):
-    user_id = request.headers.x_userid
-    user_name = request.headers.x_name
-    print(request.headers)
-    if not user_id or not user_name:
+    user_id = request.headers.get("x_userid")
+    user_name = request.headers.get("x_name")
+
+    if user_id is None or user_name is None:
         return JSONResponse(
             status_code=400,
             content={"status": "error", "message": "헤더에 토큰이 필요합니다"},
