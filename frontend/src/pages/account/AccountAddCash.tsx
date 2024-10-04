b4577fa1c4d92fcbbbe4c5d1d04d6d40b3bb5e40
이찬규 <isso8336@gmail.com>
2024-10-04 16:44:08 +0900

    :sparkles: Feat: link  addCash page to API
    <body>
    - AccountAddCash.tsx
    	- to link addCash page to API
    
    <footer>

--- frontend/src/pages/account/AccountAddCash.tsx
+++ frontend/src/pages/account/AccountAddCash.tsx
@@ -3,18 +3,40 @@ import Button, { ButtonConfigType } from '@/components/common/buttons/Button';
 import { useNavigate } from 'react-router';
 import TopBar from '@/components/layouts/TopBar';
 import { BottomTab } from '@/components/layouts/BottomTab';
+import { postUserFreeAccountAddCash } from '@/services/account';
 
 const AddCash = () => {
   const navigate = useNavigate();
 
   const [selectedButton, setSelectedButton] = useState<number | null>(null);
+  const [selectedAmount, setSelectedAmount] = useState<number | null>(null);
 
-  const handleButtonClick = (index: number) => {
+  const handleButtonClick = (index: number, amount: number) => {
     setSelectedButton(index);
+    setSelectedAmount(amount);
   };
 
-  const handleNextStep = () => {
+  const handleNextStep = (
+    accountNo: string = '1',
+    transactionBalance: number,
+    transactionSummary: string,
+  ) => {
     if (selectedButton !== null) {
+      const addCash = async () => {
+        try {
+          const response = await postUserFreeAccountAddCash(
+            accountNo,
+            transactionBalance,
+            transactionSummary,
+          );
+          if (response.status !== 200) {
+            throw new Error('Network response was not ok');
+          }
+        } catch (error) {
+          console.error('Error fetching answer:', error);
+        }
+      };
+      addCash();
       const route = '/account/check';
       navigate(route);
     }
@@ -23,6 +45,7 @@ const AddCash = () => {
   const ButtonConfig: ButtonConfigType[] = [
     {
       label: '100,000원',
+      amount: 100000,
       route: '',
       size: 'large',
       color: 'orange',
@@ -30,6 +53,7 @@ const AddCash = () => {
     },
     {
       label: '300,000원',
+      amount: 300000,
       route: '',
       size: 'large',
       color: 'orange',
@@ -37,6 +61,7 @@ const AddCash = () => {
     },
     {
       label: '500,000원',
+      amount: 500000,
       route: '',
       size: 'large',
       color: 'orange',
@@ -44,6 +69,7 @@ const AddCash = () => {
     },
     {
       label: '1,000,000원',
+      amount: 1000000,
       route: '',
       size: 'large',
       color: 'orange',
@@ -63,7 +89,7 @@ const AddCash = () => {
             label={button.label}
             size={button.size}
             color={selectedButton === index ? 'blue' : button.color}
-            onClick={() => handleButtonClick(index)}
+            onClick={() => handleButtonClick(index, button.amount!)}
             className={button.className}
           />
         ))}
@@ -71,7 +97,8 @@ const AddCash = () => {
       <div className='mt-[6vh] flex justify-center p-4'>
         <Button
           label='입금 하기'
-          onClick={handleNextStep}
+          // 임시 accountNo 하드 코딩
+          onClick={() => handleNextStep('1', selectedAmount!, '싸피 은행')}
           className='w-full max-w-[calc(100%-60px)]'
           disabled={selectedButton === null}
         />
