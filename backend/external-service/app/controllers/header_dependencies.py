f5380277b6599bdfe09d22ab3d30ec95d5187cb5
q0_u_0p <q0_u_0p@naver.com>
2024-10-04 17:55:02 +0900

    🐛 Fix: variable name error

--- backend/external-service/app/controllers/header_dependencies.py
+++ backend/external-service/app/controllers/header_dependencies.py
@@ -2,9 +2,11 @@ from urllib.parse import unquote
 
 from fastapi import Request
 from fastapi.responses import JSONResponse
+
 from ..config.logger import setup_logger
 
 logger = setup_logger("app")
+
 
 class UserHeaderInfo:
     def __init__(self, user_id: int, name: str):
@@ -15,15 +17,13 @@ class UserHeaderInfo:
 def get_user_header_info(
         request: Request,
 ):
-    user_id = request.headers.get("x_userid")
-    user_name = request.headers.get("x_name")
-
+    user_id = request.headers.get("x-userid")
+    user_name = request.headers.get("x-name")
+    logger.info(request.headers)
     if user_id is None or user_name is None:
         return JSONResponse(
             status_code=400,
             content={"status": "error", "message": "헤더에 토큰이 필요합니다"},
         )
 
-    name = unquote(user_name)  # URL 디코딩
-    user_id = int(user_id)
-    return UserHeaderInfo(id=user_id, name=user_name)
+    return UserHeaderInfo(id=int(user_id), name=unquote(user_name))
