36b3f12e6cc70f6e07759d427387aa37346feca1
이희주 <leemoon63@gmail.com>
2024-10-07 00:14:55 +0900

    :recycle: Refactor: modify mission api

--- backend/user-service/src/main/java/com/uhbooba/userservice/service/MissionService.java
+++ backend/user-service/src/main/java/com/uhbooba/userservice/service/MissionService.java
@@ -1,8 +1,11 @@
 package com.uhbooba.userservice.service;
 
+import com.uhbooba.userservice.dto.response.MissionStatusResponse;
 import com.uhbooba.userservice.entity.User;
 import com.uhbooba.userservice.exception.NotFoundException;
 import com.uhbooba.userservice.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,20 +24,30 @@ public class MissionService {
     @Transactional
     public void clearMission(String userId, int missionNumber) {
         User user = getUserByUsername(userId);
-        user.setMissionCleared(missionNumber);
+
+        user.updateMission(missionNumber);
         userRepository.save(user);
     }
 
     @Transactional(readOnly = true)
     public boolean isMissionCleared(String userId, int missionNumber) {
         User user = getUserByUsername(userId);
-        return user.isMissionCleared(missionNumber);
+        return (user.getMissionStatus() & (1 << missionNumber)) != 0;
     }
 
     @Transactional(readOnly = true)
-    public int getClearedMissionCount(String userId) {
+    public List<MissionStatusResponse> getClearedMissionCount(String userId) {
         User user = getUserByUsername(userId);
-        return user.getClearedMissionCount();
+
+        int status = user.getMissionStatus();
+        List<MissionStatusResponse> missionStatusList = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            boolean isCleared = (status & (1 << i)) != 0;
+            missionStatusList.add(MissionStatusResponse.of(i + 1, isCleared));
+        }
+
+        return missionStatusList;
     }
 
 }
