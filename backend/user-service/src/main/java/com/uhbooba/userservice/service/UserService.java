696294c4c61b25f77135352a3564fd2d6b2eb97c
김인엽 <inyub98@gmail.com>
2024-10-07 16:31:23 +0900

    :sparkles: Feat: add api for check password

--- backend/user-service/src/main/java/com/uhbooba/userservice/service/UserService.java
+++ backend/user-service/src/main/java/com/uhbooba/userservice/service/UserService.java
@@ -1,5 +1,6 @@
 package com.uhbooba.userservice.service;
 
+import com.uhbooba.userservice.dto.request.PasswordCheckRequest;
 import com.uhbooba.userservice.dto.request.SignupRequest;
 import com.uhbooba.userservice.dto.request.UpdatePasswordRequest;
 import com.uhbooba.userservice.dto.response.UserSignupMessageResponse;
@@ -23,11 +24,11 @@ public class UserService {
         duplicatePhone(request.phone());
 
         User user = User.builder()
-            .name(request.name())
-            .username(request.username())
-            .password(bCryptPasswordEncoder.encode(request.password()))
-            .phone(request.phone())
-            .build();
+                        .name(request.name())
+                        .username(request.username())
+                        .password(bCryptPasswordEncoder.encode(request.password()))
+                        .phone(request.phone())
+                        .build();
 
         User savedUser = userRepository.save(user);
         return UserSignupMessageResponse.of(savedUser);
@@ -35,26 +36,26 @@ public class UserService {
 
     public void duplicatePhone(String phone) {
         userRepository.findByPhone(phone)
-            .ifPresent(user -> {
-                throw new DuplicateUserException("이미 가입한 회원입니다.");
-            });
+                      .ifPresent(user -> {
+                          throw new DuplicateUserException("이미 가입한 회원입니다.");
+                      });
     }
 
     public void duplicateUsername(String username) {
         userRepository.findByUsername(username)
-            .ifPresent(user -> {
-                throw new DuplicateUserException("이미 존재하는 아이디입니다.");
-            });
+                      .ifPresent(user -> {
+                          throw new DuplicateUserException("이미 존재하는 아이디입니다.");
+                      });
     }
 
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username)
-            .orElseThrow(() -> new NotFoundException(username + "가 존재하지 않습니다."));
+                             .orElseThrow(() -> new NotFoundException(username + "가 존재하지 않습니다."));
     }
 
     private void getUserByPhone(String phone) {
         User user = userRepository.findByPhone(phone)
-            .orElseThrow(() -> new NotFoundException(phone + "가 존재하지 않습니다."));
+                                  .orElseThrow(() -> new NotFoundException(phone + "가 존재하지 않습니다."));
 
     }
 
@@ -63,7 +64,14 @@ public class UserService {
         getUserByPhone(request.phone());
 
         userRepository.updatePasswordByPhone(request.phone(),
-            bCryptPasswordEncoder.encode(request.password()));
+                                             bCryptPasswordEncoder.encode(request.password()));
     }
 
+    public Boolean isValidPassword(PasswordCheckRequest request) {
+        // 1. user 찾기
+        User user = userRepository.findById(request.id())
+                                  .orElseThrow(() -> new NotFoundException("사용자를 찾을 수 없습니다."));
+        // 2. 입력한 비밀번호와 저장된 해시된 비밀번호 비교
+        return bCryptPasswordEncoder.matches(request.password(), user.getPassword());
+    }
 }
