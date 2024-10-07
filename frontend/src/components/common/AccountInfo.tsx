18d13dbece37a57a8a6cb00f7d9201e9596d3052
swybean <swybean@gmail.com>
2024-10-07 15:59:20 +0900

    :recycle: Refactor: 코드리뷰 반영

--- frontend/src/components/common/AccountInfo.tsx
+++ frontend/src/components/common/AccountInfo.tsx
@@ -26,16 +26,16 @@ const AccountInfo: React.FC<AccountInfoProps> = ({
       <div className='mb-[5px] ml-[20px] mt-[10px] font-bold'>
         <div className='mr-2 flex justify-between'>
           <div className='text-[24px] text-[#5A6A59]'>{accountType}</div>
+        </div>
+        <div className='mb-[6px] text-[16px]'>{accountNumber}</div>
+        <div className='text-[28px]'>잔액 {amount.toLocaleString()}원</div>
+        <div className='mr-4 flex justify-center'>
           <Button
             label={buttonName}
             color='lightOrange'
             size='customMedium'
             onClick={onClick ? onClick : () => navigate(moveTo)}
           />
-        </div>
-        <div className='mb-[6px] text-[16px]'>{accountNumber}</div>
-        <div className='text-[28px]'>
-          잔액 {Number(amount).toLocaleString()}원
         </div>
       </div>
     </div>
