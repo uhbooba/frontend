716c9d8960aeea1273256e4d28a24c95598f4ba0
김지윤 <yunva17@naver.com>
2024-10-08 04:12:50 +0900

    :Recycle: Refactor: 레이아웃 수정

--- src/components/common/LevelBar.tsx
+++ src/components/common/LevelBar.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
 
 type LevelBarProps = {
-    currentLevel?: number;
-    totalLevel?: number;
+  currentLevel?: number;
+  totalLevel?: number;
 };
 
 // LevelBar 사용법 1. currentLevel에 현재 단계, totalLevel에 전체 단계 값을 입력해서 사용하면 됩니다.
-const LevelBar: React.FC<LevelBarProps> = ({ currentLevel = 1, totalLevel =  5}) => {
-    const levels = totalLevel > 0 ? Array.from({ length: totalLevel }) : [];
+const LevelBar: React.FC<LevelBarProps> = ({
+  currentLevel = 1,
+  totalLevel = 5,
+}) => {
+  const levels = totalLevel > 0 ? Array.from({ length: totalLevel }) : [];
 
-    return (
-        <div className='text-center'>
-            <div className='flex justify-center items-center'>
-                {levels.map((_, index) => (
-                    <div key={index}>
-                        <div>{index+1}</div>
-                        {index+1 === currentLevel ? (
-                            <div className='w-16 h-2 mx-1 bg-gray-300 rounded-full overflow-hidden flex'>
-                                <div className='w-1/2 h-full rounded-full bg-[#FFAF2A]' /> 
-                                <div className='w-1/2 h-full' /> 
-                            </ div>
-                        ) : (
-                            <div className={`w-16 h-2 mx-1 rounded-full ${index < currentLevel ? 'bg-[#FFAF2A]' : 'bg-gray-300'}`} />   
-                        )}
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className='mb-8 mt-4 text-center'>
+      <div className='flex items-center justify-center'>
+        {levels.map((_, index) => (
+          <div key={index}>
+            <div>{index + 1}</div>
+            {index + 1 === currentLevel ? (
+              <div className='mx-1 flex h-2 w-16 overflow-hidden rounded-full bg-gray-300'>
+                <div className='h-full w-1/2 rounded-full bg-[#FFAF2A]' />
+                <div className='h-full w-1/2' />
+              </div>
+            ) : (
+              <div
+                className={`mx-1 h-2 w-16 rounded-full ${index < currentLevel ? 'bg-[#FFAF2A]' : 'bg-gray-300'}`}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LevelBar;
