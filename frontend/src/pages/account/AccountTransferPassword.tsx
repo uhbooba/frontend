2ca137027b07ef04e66d3f359bc0281191e012ee
이찬규 <isso8336@gmail.com>
2024-10-07 15:27:30 +0900

    :sparkles: Feat: add parameters for transfer API
    <body>
    - AccountTransferPassword.tsx
    	- to add parameters for post request in account transfer
    
    <footer>

--- frontend/src/pages/account/AccountTransferPassword.tsx
+++ frontend/src/pages/account/AccountTransferPassword.tsx
@@ -2,12 +2,47 @@ import { useNavigate } from 'react-router';
 import LevelBar from '@/components/common/LevelBar';
 import PasswordInput from '@/components/common/PasswordInput';
 import TopBar from '@/components/layouts/TopBar';
+import { postUserFreeAccountTransfer } from '@/services/account';
+import { useAtom } from 'jotai';
+import {
+  transactionBalanceAtom,
+  depositAccountNoAtom,
+  depositTransactionSummaryAtom,
+  withdrawalAccountNoAtom,
+  withdrawalTransactionSummaryAtom,
+} from '@/atoms/account/accountTransferAtoms';
 
 const AccountTransferPassword = () => {
   const navigate = useNavigate();
+  const [depositAccountNo] = useAtom(depositAccountNoAtom);
+  const [depositTransactionSummary] = useAtom(depositTransactionSummaryAtom);
+  const [transactionBalance] = useAtom(transactionBalanceAtom);
+  const [withdrawalAccountNo] = useAtom(withdrawalAccountNoAtom);
+  const [withdrawalTransactionSummary] = useAtom(
+    withdrawalTransactionSummaryAtom,
+  );
 
   const passwordComplete = (password: string[]) => {
     console.log('비밀번호 확인용 :', password.join(''));
+    const transfer = () => {
+      try {
+        postUserFreeAccountTransfer(
+          // 이체받을 계좌번호
+          depositAccountNo,
+          // 이체받을 계좌기록
+          depositTransactionSummary,
+          // 이체 금액
+          transactionBalance,
+          // 출금할 계좌 번호
+          withdrawalAccountNo,
+          // 출금할 계좌 기록
+          withdrawalTransactionSummary,
+        );
+      } catch (error) {
+        console.error('Error fetching answer:', error);
+      }
+    };
+    transfer();
     navigate('/account/transfer/success');
   };
 
