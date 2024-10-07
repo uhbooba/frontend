b35175766df4c2504180608d024e1df1b13a463c
swybean <swybean@gmail.com>
2024-10-07 14:21:56 +0900

    :Feat: sparkles: 메인화면 예적금 버튼, 미션api, 중도해지 api 등
    <body>
    - 메인화면 예적금 버튼 내용 제작
    - 예적금 중도해지 api 이용 새로고침 오류 해결
    - 미션 api 이용, 미션 클리어 여부에 따른 페이지 변동

--- src/pages/depositCancel/CancelDepositMission.tsx
+++ src/pages/depositCancel/CancelDepositMission.tsx
@@ -1,19 +1,19 @@
-import Button from '@/components/common/buttons/Button';
-import { useNavigate } from 'react-router';
 import { BottomTab } from '@/components/layouts/BottomTab';
+import Button from '@/components/common/buttons/Button';
 import TopBar from '@/components/layouts/TopBar';
+import { useNavigate } from 'react-router';
 
-const SavingsMission = () => {
+const CancelDepositMission = () => {
   const navigate = useNavigate();
 
-  const GoExplain = () => {
-    navigate('/savings');
+  const GoNext = () => {
+    navigate('/cancel/deposit/explain');
   };
 
   return (
     <div className='min-h-screen bg-yellow-100'>
       <div className='fixed left-0 top-0 z-10 w-full'>
-        <TopBar title='적금 가입' />
+        <TopBar title='예금 중도해지' />
       </div>
 
       {/* 돼지 말풍선 부분 */}
@@ -21,26 +21,25 @@ const SavingsMission = () => {
         {/* 말풍선 스타일 적용 */}
         <div className='relative mt-20 h-[440px] w-[360px] rounded-lg bg-white'>
           <div className='mt-8 text-center'>
-            <p className='text-4xl font-bold'>4단계 미션</p> <br />
+            <p className='text-4xl font-bold'>5단계 미션</p> <br />
             <p className='text-2xl'>
               이수자 할아버지는 <br />
-              목돈을 마련하기 위해서 <br />
-              매월 자동납부를 이용한 <br />
-              <span className='text-yellow-400'>적금상품</span>을 가입하려고
-              합니다.
+              큰 돈이 필요해져서 <br />
+              가입했던 예금상품을 <br />
+              <span className='text-yellow-400'>중도해지</span> 하려고 합니다.
             </p>
             <br />
             <p className='text-4xl font-bold'>
-              3가지 상품 중에서 <br />
-              적금상품 하나를 <br />
-              가입해보세요!
+              가입하셨던 <br />
+              예금 상품을 <br />
+              중도해지 해보세요!
             </p>
             <div className='pl-8 pr-8'>
               <Button
-                label='가입하러가기'
+                label='중도해지 하러가기'
                 size='small'
                 color='orange'
-                onClick={() => GoExplain()}
+                onClick={() => GoNext()}
                 className='mt-4'
               />
             </div>
@@ -54,7 +53,7 @@ const SavingsMission = () => {
         <div className='flex items-center justify-center'>
           <img
             src='/assets/images/pig.png'
-            alt='pig'
+            alt='Pig'
             className='mt-4 h-56 w-56'
           />
         </div>
@@ -67,4 +66,4 @@ const SavingsMission = () => {
   );
 };
 
-export default SavingsMission;
+export default CancelDepositMission;
