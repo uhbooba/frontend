8eecc0e9fbbe5d36286cb5435072ec709ec1c496
이희주 <leemoon63@gmail.com>
2024-10-08 16:49:40 +0900

    :sparkles: Feat: imnplement fcm permission logic

--- src/services/notification.ts
+++ src/services/notification.ts
@@ -1,19 +1,9 @@
-import { axiosInstance } from '@/utils/axiosInstance';
 import { messaging } from '@/firebase-message';
 import { getToken, onMessage } from 'firebase/messaging';
 
-export const postNotificationToken = async (token: string) => {
-  const bodyData = { token };
-
-  const response = await axiosInstance.post(
-    '/user-service/notification',
-    bodyData,
-  );
-  return response;
-};
-
-// 알림 권한 요청 및 토큰 처리
-export const requestNotificationPermission = async () => {
+export const requestNotificationPermission = async (): Promise<
+  string | null
+> => {
   const permission = await Notification.requestPermission();
 
   if (permission === 'granted') {
@@ -22,28 +12,20 @@ export const requestNotificationPermission = async () => {
     });
 
     if (currentToken) {
-      console.log('FCM Token:', currentToken);
-      return postNotificationToken(currentToken); // 서버로 토큰 전송
+      return currentToken;
     } else {
-      console.warn('토큰을 가져올 수 없습니다. 권한을 허용해주세요.');
+      throw new Error('FCM 토큰을 가져올 수 없습니다.');
     }
   } else {
-    console.warn(
-      '알림 권한이 거부되었습니다. 알림을 사용하려면 권한을 허용해주세요.',
-    );
+    throw new Error('알림 권한이 거부되었습니다.');
   }
 };
 
-// 포그라운드 푸시 알림 수신 설정
-export const setupForegroundMessageHandler = () => {
+export const setupForegroundMessageHandler = (): void => {
   onMessage(messaging, (payload) => {
-    console.log('포그라운드에서 푸시 알림 수신:', payload);
-
     const { title, body } = payload.notification || {};
     if (title && body) {
-      new Notification(title, {
-        body,
-      });
+      new Notification(title, { body });
     }
   });
 };
