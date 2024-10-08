36f976b93279399c17d5663ba228cd178100eec4
이희주 <leemoon63@gmail.com>
2024-10-08 12:50:55 +0900

    :hammer: Chore: set firebase cloud message

---
+++ backend/user-service/src/main/java/com/uhbooba/userservice/config/FirebaseConfig.java
@@ -0,0 +1,31 @@
+package com.uhbooba.userservice.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
