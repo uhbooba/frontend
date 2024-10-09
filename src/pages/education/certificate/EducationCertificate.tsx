9920cd44b9e62d83c05bd98596327536816601b0
swybean <swybean@gmail.com>
2024-10-09 18:03:51 +0900

    :sparkles: Feat: 이수증 전체 미션 api 연결
    <body>
    - EducationCerificate.tsx
    	- 스탬프 개수 모든 미션 확인 api로 연동
    	- 스탬프 7개 모아야지 이수증 발급 버튼 이동 가능

--- src/pages/education/certificate/EducationCertificate.tsx
+++ src/pages/education/certificate/EducationCertificate.tsx
@@ -3,16 +3,53 @@ import TextBubble from '@/components/common/TextBubble';
 import MainWrapper from '@/components/layouts/MainWrapper';
 import TopBar from '@/components/layouts/TopBar';
 import { useNavigate } from 'react-router';
+import { useState, useEffect } from 'react';
+import { getMissionsClearStatus } from '@/services/mission';
+import NoModal from '@/components/modals/NoModal';
 
 const EducationCertificate = () => {
   const navigate = useNavigate();
+  const [clearedMissions, setClearedMissions] = useState(0);
+  const [remainingMissions, setRemainingMissions] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    // 모든 미션 클리어 확인하는 api 부르기
+    const fetchMissionsStatus = async () => {
+      try {
+        const response = await getMissionsClearStatus();
+        const completedMissions = response.result.filter(
+          (mission) => mission.isCleared,
+        ).length;
+        const incompleteMissions = response.result.filter(
+          (mission) => !mission.isCleared,
+        ).length;
+
+        setClearedMissions(completedMissions); // true 미션 개수
+        setRemainingMissions(incompleteMissions); // false 미션 개수
+      } catch (error) {
+        console.error('getMissionsClearStatus 에러', error);
+      }
+    };
+
+    fetchMissionsStatus();
+  }, []);
 
   const GoNext = () => {
-    navigate('/education/writing');
+    if (clearedMissions === 7) {
+      // 개발테스트 할 때는 이거 7 대신 자기 미션 완료 개수로 바꾸면 다음 페이지 볼 수 있음
+      navigate('/education/writing');
+    } else {
+      setIsModalOpen(true);
+    }
   };
 
   const GoEdu = () => {
     navigate('/education');
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -24,8 +61,8 @@ const EducationCertificate = () => {
             content={[
               '스탬프 7개를 모두 모으면',
               '이수증을 발급할 수 있습니다.',
-              '현재 모은 스탬프는 @개입니다.',
-              '스탬프 #개를 더 모아야합니다.',
+              `현재 모은 스탬프는 ${clearedMissions}개입니다.`,
+              `스탬프 ${remainingMissions}개를 더 모아야합니다.`,
             ]}
             tailPosition='right'
             textPosition='left'
@@ -48,6 +85,14 @@ const EducationCertificate = () => {
             onClick={() => GoNext()}
           />
         </div>
+
+        <NoModal
+          isOpen={isModalOpen}
+          ModalClose={closeModal}
+          imageSrc='/assets/icons/warning.png'
+          title='스탬프가 부족해요!'
+          description='스탬프 7개를 모두 모아야합니다.'
+        />
       </MainWrapper>
     </div>
   );
