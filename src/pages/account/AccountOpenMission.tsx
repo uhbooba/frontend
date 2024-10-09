72d5cc7b6c681ff3fe4d8e26e79a35112884bf08
이찬규 <isso8336@gmail.com>
2024-10-10 01:52:48 +0900

    :sparkles: Feat: AccountOpenMission skeleton -ing
    <body>
    - AccountOpenMission.tsx
    	- AccountOpenMission skeleton - ing
    - AccountOpenMissionSuccess.tsx
    	- AccountOpenMission skeleton - ing
    
    <footer>

--- src/pages/account/AccountOpenMission.tsx
+++ src/pages/account/AccountOpenMission.tsx
@@ -1,68 +1,70 @@
 import Button from '@/components/common/buttons/Button';
 import { useNavigate } from 'react-router';
-import { BottomTab } from '@/components/layouts/BottomTab';
 import TopBar from '@/components/layouts/TopBar';
+import TextBubble from '@/components/common/TextBubble';
+import MainWrapper from '@/components/layouts/MainWrapper';
+import { exchangeMissionAtom } from '@/atoms/exchangeAtoms';
+import { useSetAtom } from 'jotai';
+import { useEffect } from 'react';
 
 const AccountOpenMission = () => {
   const navigate = useNavigate();
+  const setIsMission = useSetAtom(exchangeMissionAtom);
+
+  useEffect(() => {
+    // 페이지에 들어오면 미션 상태 true
+    setIsMission(true);
+  }, []);
 
   const GoExplain = () => {
-    navigate('/savings');
+    navigate('/account/products');
   };
 
   return (
-    <div className='min-h-screen bg-yellow-100'>
-      <div className='fixed left-0 top-0 z-10 w-full'>
-        <TopBar title='계좌 생성' />
-      </div>
-
-      {/* 돼지 말풍선 부분 */}
-      <div className='relative mt-8 flex flex-col items-center'>
-        {/* 말풍선 스타일 적용 */}
-        <div className='relative mt-20 h-[440px] w-[360px] rounded-lg bg-white'>
-          <div className='mt-8 text-center'>
-            <p className='text-4xl font-bold'>2단계 미션</p> <br />
-            <p className='text-2xl'>
-              이수자 할아버지는 <br />
-              경로당에서 사용할 <br />
-              새 계좌를 만들려고 합니다. <br />
-              경로당 모임비를 <br />
-              따로 관리하기 위한 <br />
-              <span className='text-yellow-400'>새로운 계좌</span>가 필요합니다.
-            </p>
-            <br />
-            <p className='text-4xl font-bold'>
-              수시입출금 계좌를 <br />
-              가입해보세요!
-            </p>
-            <div className='pl-8 pr-8'>
-              <Button
-                label='가입하러가기'
-                size='small'
-                color='orange'
-                onClick={() => GoExplain()}
-                className='mt-4'
-              />
-            </div>
-          </div>
-
-          {/* 말풍선 꼬리 부분 */}
-          <div className='absolute bottom-[-29px] left-[155px] h-0 w-0 border-l-[30px] border-r-[30px] border-t-[30px] border-solid border-l-transparent border-r-transparent border-t-white'></div>
-        </div>
-
-        {/* 돼지 이미지 */}
+    <div className='flex min-h-screen flex-col bg-yellow-100'>
+      <TopBar title='계좌 개설' />
+      <MainWrapper className='flex flex-col items-center justify-center'>
+        <TextBubble
+          bubbleSize='w-11/12'
+          content={
+            <>
+              <p className='text-4xl font-bold'>2단계 미션</p> <br />
+              <p className='text-2xl'>
+                이수자 할아버지는 <br /> 경로당에서 사용할 <br />
+                새 계좌를 만들려고 합니다. <br />
+                <br />
+                경로당 모임비를 따로 <br />
+                관리하기 위한 <br />
+                <span className='text-bold text-primary'>수시 입출금</span>{' '}
+                계좌가 <br />
+                필요합니다.
+              </p>
+              <br />
+              <p className='text-4xl font-bold'>
+                수시 입출금 계좌를 <br />
+                만드세요!
+              </p>
+              <div className='ml-8 mr-8'>
+                <Button
+                  label='계좌 만들러 가기'
+                  size='small'
+                  color='orange'
+                  onClick={() => GoExplain()}
+                  className='mt-4'
+                />
+              </div>
+            </>
+          }
+        />
         <div className='flex items-center justify-center'>
           <img
             src='/assets/images/pig.png'
-            alt='pig'
+            alt='Pig'
             className='mt-4 h-56 w-56'
+            loading='lazy'
           />
         </div>
-      </div>
-
-      <div className='fixed bottom-0 left-0 w-full'>
-        <BottomTab />
-      </div>
+      </MainWrapper>
     </div>
   );
 };
