a78b471fc9c4575ada02b03b9c31af27e44d62f7
swybean <swybean@gmail.com>
2024-10-09 17:52:18 +0900

    :recycle: Refactor: 비밀번호 로직 수정 및 예적금 중도해지 비밀번호 인증 추가

--- frontend/src/pages/deposit/DepositSuccess.tsx
+++ frontend/src/pages/deposit/DepositSuccess.tsx
@@ -1,101 +1,101 @@
 import Button from '@/components/common/buttons/Button';
 import TopBar from '@/components/layouts/TopBar';
 import { useNavigate } from 'react-router';
-import { createDepositAccount, getDepositProducts } from '@/services/deposit';
-import { useAtom } from 'jotai';
-import { useEffect, useState } from 'react';
-import {
-  depositAccountAtom,
-  selectedDepositProductAtom,
-  withdrawalAccountAtom,
-  selectMoneyAtom,
-  depositPasswordAtom,
-} from '@/atoms/deposit/depositDataAtoms';
+// import { createDepositAccount, getDepositProducts } from '@/services/deposit';
+// import { useAtom } from 'jotai';
+// import { useEffect, useState } from 'react';
+// import {
+//   depositAccountAtom,
+//   selectedDepositProductAtom,
+//   withdrawalAccountAtom,
+//   selectMoneyAtom,
+//   depositPasswordAtom,
+// } from '@/atoms/deposit/depositDataAtoms';
 import MainWrapper from '@/components/layouts/MainWrapper';
 
 const DepositSuccess = () => {
   const navigate = useNavigate();
-  const [selectedDepositProduct] = useAtom(selectedDepositProductAtom); // 예금상품명 가져오기
-  const [, setDepositAccount] = useAtom(depositAccountAtom); // 예금 계좌정보 저장할곳
-  const [withdrawalAccount] = useAtom(withdrawalAccountAtom); // 출금계좌 정보 가져올것
-  const [selectMoney] = useAtom(selectMoneyAtom); // 예치 금액 가져오기
-  const [accountTypeUniqueNo, setAccountTypeUniqueNo] = useState<string | null>(
-    null,
-  ); // 사용자가 고른 상품과 동일한 이름의 예금상품 유니크이름 저장하기
-  const [depositPassword] = useAtom(depositPasswordAtom); // 패스워드아톰
+  // const [selectedDepositProduct] = useAtom(selectedDepositProductAtom); // 예금상품명 가져오기
+  // const [, setDepositAccount] = useAtom(depositAccountAtom); // 예금 계좌정보 저장할곳
+  // const [withdrawalAccount] = useAtom(withdrawalAccountAtom); // 출금계좌 정보 가져올것
+  // const [selectMoney] = useAtom(selectMoneyAtom); // 예치 금액 가져오기
+  // const [accountTypeUniqueNo, setAccountTypeUniqueNo] = useState<string | null>(
+  //   null,
+  // ); // 사용자가 고른 상품과 동일한 이름의 예금상품 유니크이름 저장하기
+  // const [depositPassword] = useAtom(depositPasswordAtom); // 패스워드아톰
 
-  // 예금상품전체조회api부터 해야함
-  useEffect(() => {
-    const fetchAccountTypeUniqueNo = async () => {
-      if (!selectedDepositProduct) {
-        console.error('선택된 예금 상품이 없습니다.');
-        return;
-      }
+  // // 예금상품전체조회api부터 해야함
+  // useEffect(() => {
+  //   const fetchAccountTypeUniqueNo = async () => {
+  //     if (!selectedDepositProduct) {
+  //       console.error('선택된 예금 상품이 없습니다.');
+  //       return;
+  //     }
 
-      try {
-        const response = await getDepositProducts();
-        const products = response?.data?.result || [];
+  //     try {
+  //       const response = await getDepositProducts();
+  //       const products = response?.data?.result || [];
 
-        // 선택된 상품명과 일치하는 상품 정보 찾기
-        const selectedProductInfo = products.find(
-          (product: any) => product.accountName === selectedDepositProduct.name,
-        );
+  //       // 선택된 상품명과 일치하는 상품 정보 찾기
+  //       const selectedProductInfo = products.find(
+  //         (product: any) => product.accountName === selectedDepositProduct.name,
+  //       );
 
-        if (selectedProductInfo) {
-          setAccountTypeUniqueNo(selectedProductInfo.accountTypeUniqueNo); // 선택된 상품의 고유 번호 저장
-        }
-      } catch (error) {
-        console.error('api 호출 자체 에러', error);
-      }
-    };
+  //       if (selectedProductInfo) {
+  //         setAccountTypeUniqueNo(selectedProductInfo.accountTypeUniqueNo); // 선택된 상품의 고유 번호 저장
+  //       }
+  //     } catch (error) {
+  //       console.error('api 호출 자체 에러', error);
+  //     }
+  //   };
 
-    fetchAccountTypeUniqueNo();
-  }, [selectedDepositProduct]);
+  //   fetchAccountTypeUniqueNo();
+  // }, [selectedDepositProduct]);
 
-  // 이건 예금계좌 생성
-  useEffect(() => {
-    const createAccount = async () => {
-      if (!selectedDepositProduct) {
-        console.error('선택된 예금 상품이 없습니다.');
-        return;
-      }
+  // // 이건 예금계좌 생성
+  // useEffect(() => {
+  //   const createAccount = async () => {
+  //     if (!selectedDepositProduct) {
+  //       console.error('선택된 예금 상품이 없습니다.');
+  //       return;
+  //     }
 
-      try {
-        if (!selectMoney) {
-          console.error('예치 금액이 없습니다.');
-          return;
-        }
+  //     try {
+  //       if (!selectMoney) {
+  //         console.error('예치 금액이 없습니다.');
+  //         return;
+  //       }
 
-        // 금액버튼 문자열이니까 쉼표도 없애고 숫자로 바꿔줘야됨
-        const depositBalance = parseInt(selectMoney.replace(/,/g, ''), 10);
+  //       // 금액버튼 문자열이니까 쉼표도 없애고 숫자로 바꿔줘야됨
+  //       const depositBalance = parseInt(selectMoney.replace(/,/g, ''), 10);
 
-        // 예금계좌 생성 api 호출
-        const response = await createDepositAccount(
-          withdrawalAccount!.accountNo,
-          accountTypeUniqueNo!,
-          depositBalance,
-          depositPassword,
-        );
+  //       // 예금계좌 생성 api 호출
+  //       const response = await createDepositAccount(
+  //         withdrawalAccount!.accountNo,
+  //         accountTypeUniqueNo!,
+  //         depositBalance,
+  //         depositPassword,
+  //       );
 
-        if (response?.data?.result) {
-          setDepositAccount(response.data.result);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  //       if (response?.data?.result) {
+  //         setDepositAccount(response.data.result);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-    if (accountTypeUniqueNo) {
-      createAccount(); // 고유번호 가져왔으면 이제 예금 계좌 생성 api 쓰기
-    }
-  }, [
-    setDepositAccount,
-    selectedDepositProduct,
-    withdrawalAccount,
-    selectMoney,
-    accountTypeUniqueNo,
-    depositPassword,
-  ]);
+  //   if (accountTypeUniqueNo) {
+  //     createAccount(); // 고유번호 가져왔으면 이제 예금 계좌 생성 api 쓰기
+  //   }
+  // }, [
+  //   setDepositAccount,
+  //   selectedDepositProduct,
+  //   withdrawalAccount,
+  //   selectMoney,
+  //   accountTypeUniqueNo,
+  //   depositPassword,
+  // ]);
 
   const GoNext = () => {
     navigate('/');
