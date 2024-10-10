ab011bd761b4eace8f083e2e99989dba2d5b84ba
이희주 <leemoon63@gmail.com>
2024-10-10 18:27:38 +0900

    :sparkles: Feat: add fcm url

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -11,13 +11,13 @@ self.addEventListener('push', function (e) {
   if (!e.data.json()) return;
 
   const resultData = e.data.json().notification;
-  // const resultURL = e.data.json().data.click_action;
+  const resultURL = 'https://j11a402.q.ssafy.io/';
 
   const notificationTitle = resultData.title;
   const notificationOptions = {
     body: resultData.body,
     icon: 'https://s3.youm.me/uhbooba/icons/pig_face.png',
-    // data: { click_action: resultURL },
+    data: { click_action: resultURL },
   };
 
   console.log('notificationTitle', notificationTitle);
@@ -25,8 +25,23 @@ self.addEventListener('push', function (e) {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// self.addEventListener('notificationclick', function (event) {
-//   const resultURL = event.notification.data.click.action;
-//   event.waitUntil(clients.openWindow(resultURL));
-//   event.notification.close();
-// });
+self.addEventListener('notificationclick', function (event) {
+  const resultURL = event.notification.data.click_action;
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then(function (clientList) {
+        for (const client of clientList) {
+          if (client.url === resultURL && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(resultURL);
+        }
+      }),
+  );
+
+  event.notification.close();
+});
